d165d083b4732b2719bddc26004b5eed8cc4a3ce
TL <TL@gmail.com>
2022-04-24 20:27:32 +0530

    m2 Utility

--- DemoProj1/src/com/st/beans/Utility.java
+++ DemoProj1/src/com/st/beans/Utility.java
@@ -13,4 +13,8 @@ public class Utility {
 		System.out.println("Utility m1() methods...1234");
 		System.out.println("abc123");
 	}
+	
+	public void m2() {
+		System.out.println("Utility.m2");
+	}
 }	
